84c972a5471f14aafdfd03329c0b4a8619b286b7
Bridget Almas <balmas@gmail.com>
2017-02-06 20:46:36 +0000

    get rid of things we aren't using

--- perseids-client-apps/app/configurator.py
+++ perseids-client-apps/app/configurator.py
@@ -1,17 +1,9 @@
-from app.configurations.treebank import treebank
-from app.configurations.session import session
-from app.configurations.alignment import alignment
-from app.configurations.cts import cts
 from app.configurations.language import language
 from app.configurations.modules import modules
 
 
 # Instead of json, we are going the py road so we can comment configurations and lessen the load time as well...
 config = {}
-config["treebank"] = treebank
-config["alignment"] = alignment
-config["session"] = session
-config["cts"] = cts
 config["language"] = language
 config["modules"] = modules
 
